ff07672b4d1956f9314e6491464f27865825c599
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-09-16 20:43:34 +0800

    connected with substrate chain

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "semantic-ui-css/semantic.min.css";
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Navbar from './components/Navbar';
-
+import chain from './api/chain' ;
 
 function Index() {
   return <h2>Home</h2>;
@@ -18,7 +18,23 @@ function Users() {
   return <h2>Users</h2>;
 }
 
+const CASTOR_PROVIDER = "ws://127.0.0.1:9944";
+// const CASTOR_PROVIDER = 'wss://polkadot:9944';
+chain.init(CASTOR_PROVIDER, run);
+function run() {
+  chain.connect();
+  chain.getBalance("5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY", function(
+    balance
+  ) {
+    console.log(balance);
+  });
+  const keys = chain.getKeysFromUri("//Alice");
+  console.log(keys.address);
+
+}
+
 function App() {
+  
   return (
     <Router>
       <div>
---
+++ src/api/chain.js
@@ -0,0 +1,151 @@
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { Keyring } from "@polkadot/keyring";
+import { stringToU8a } from "@polkadot/util";
+// import BN from 'bn.js';
+
+let api = null;
+
+// https://substrate.dev/docs/en/tutorials/tcr/building-a-ui-for-the-tcr-runtime
+// https://polkadot.js.org/api/api/#registering-custom-types
+// https://github.com/substrate-developer-hub/substrate-tcr-ui/blob/master/src/services/tcrService.js
+
+const init = async (wsp, callback) => {
+  const provider = new WsProvider(wsp);
+
+  api = await ApiPromise.create({
+    types: {
+      ContentHash: "[u8; 32]",
+      NodeType: "u32",
+      Node: {
+        id: "ContentHash",
+        node_type: "NodeType",
+        sources: "Vec<ContentHash>"
+      },
+      GeId: "u64",
+      ActionId: "u64",
+      TcxId: "u64",
+      GovernanceEntity: {
+        threshold: "u64",
+        min_deposit: "Balance",
+        apply_stage_len: "Moment",
+        commit_stage_len: "Moment"
+      },
+      Challenge: {
+        amount: "Balance",
+        voting_ends: "Moment",
+        resolved: "bool",
+        reward_pool: "Balance",
+        total_tokens: "Balance",
+        owner: "AccountId"
+      },
+      ListingId: "u64",
+      ChallengeId: "u64",
+      Listing: {
+        id: "ListingId",
+        node_id: "ContentHash",
+        amount: "Balance",
+        application_expiry: "Moment",
+        whitelisted: "bool",
+        challenge_id: "ChallengeId",
+        owner: "AccountId"
+      },
+      Poll: {
+        votes_for: "Balance",
+        votes_against: "Balance",
+        passed: "bool"
+      },
+      Tcx: {
+        tcx_type: "u64"
+      },
+      TcxType: "u64",
+      Link: {
+        source: "u32",
+        target: "u32"
+      },
+      VecContentHash: "Vec<ContentHash>"
+    },
+    provider
+  });
+
+  // api.rpc.chain.subscribeNewHeads(header => {
+  //   console.log(`Chain is at #${header.number}`)
+  // });
+
+  callback(api);
+};
+
+const getBalance = async (address, callback) => {
+  const currentBalance = await api.query.balances.freeBalance(address);
+  callback(currentBalance.toString());
+};
+
+const getBalances = async (addresses, callback) => {
+  const currentBalances = await api.query.balances.freeBalance.multi(addresses);
+  const balancesMap = {};
+  currentBalances.forEach((item, index) => {
+    balancesMap[addresses[index]] = item.toString();
+  });
+  callback(balancesMap);
+};
+
+const transfer = async (keyring, addressFrom, addressTo, amount) => {
+  const fromPair = keyring.getPair(addressFrom);
+  api.tx.balances
+    .transfer(addressTo, amount)
+    .signAndSend(fromPair, ({ status }) => {
+      if (status.isFinalized) {
+        console.log(
+          `Completed at block hash #${status.asFinalized.toString()}`
+        );
+      } else {
+        console.log(`Current transfer status: ${status.type}`);
+      }
+    })
+    .catch(e => {
+      console.log(":( transaction failed");
+      console.error("ERROR:", e);
+    });
+};
+
+const getKeysFromSeed = _seed => {
+  if (!_seed) {
+    throw new Error("Seed not valid.");
+  }
+
+  const keyring = new Keyring({ type: "sr25519" });
+  const paddedSeed = _seed.padEnd(32);
+  return keyring.addFromSeed(stringToU8a(paddedSeed));
+};
+
+const getKeysFromUri = uri => {
+  if (!uri) {
+    throw new Error("Uri not valid.");
+  }
+
+  const keyring = new Keyring({ type: "sr25519" });
+  return keyring.addFromUri(uri);
+};
+
+const connect = async () => {
+  const [chain, name, version] = await Promise.all([
+    api.rpc.system.chain(),
+    api.rpc.system.name(),
+    api.rpc.system.version()
+  ]);
+
+  const connected = `You are connected to chain ${chain} using ${name} v${version}`;
+  console.log(connected);
+  return { chain, name, version };
+};
+
+
+export default {
+  init,
+  getBalance,
+  getBalances,
+  transfer,
+  getKeysFromSeed,
+  getKeysFromUri,
+  // ------- tcr
+  connect,
+};
